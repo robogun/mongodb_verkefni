ec6e17333abc0532dafd69e3c5a74c16bfa1acb1
robogun <32485165+robogun@users.noreply.github.com>
2020-04-29 17:30:14 +0000

    userlist

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const app = require('express')();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
-// const mongo = require('mongodb').MongoClient;
+const mongo = require('mongodb').MongoClient;
 
 var port = process.env.PORT || 3000 // Skilgreinum port
+var dburl = "mongodb://127.0.0.1/";
 
 // gerum breytu sem geymir fjölda notenda
 var fjoldi_notenda = 0;
@@ -27,17 +28,30 @@ app.get('/*', function(req, res)
     res.end();
 });
 
-// tengjumst gagnagrunninum og náum í safnið mongodb_Synidaemi 
-// en þar munum við geyma skjölin okkar
-// mongo.connect('mongodb://127.0.0.1/chatserver2020', {useUnifiedTopology: true}, function(err, db)
-// {
-//     if (err)
-//     {
-//         throw err;
-//     }
-//     var chatdb = db.db("coolchat");
-//     // hlustum eftir connection atburð á servernum og
-//     // skrifum svarfall fyrir þann atburð
+function chatlog()
+{
+    mongo.connect(dburl, function(err, db)
+    {
+        if (err) throw err;
+        var dbo = db.db("mongodb_chat");
+        dbo.collection("messages").find({}, { projection: { _id: 0, msg: 1 } }).toArray(function(err, result)
+        {
+            if (err) throw err;
+            console.log(result[2].msg);
+            db.close();
+        });
+    });
+};
+
+mongo.connect('mongodb://127.0.0.1/', {useUnifiedTopology: true}, function(err, db)
+{
+    if (err)
+    {
+        throw err;
+    }
+    var chatdb = db.db("mongodb_chat");
+    // hlustum eftir connection atburð á servernum og
+    // skrifum svarfall fyrir þann atburð
 
     io.on('connection', function(socket)
     {
@@ -50,20 +64,35 @@ app.get('/*', function(req, res)
         // láta hana sjást einhversstaðar
         io.emit('count', fjoldi_notenda);
         
-        socket.on('join', function(nick)
+        socket.on('join', function(nick, msg)
         {
             socket.userName = nick;
             // bætum nafninu við fylkið okkar
             notendur.push(nick);
+///////////////////////////////////////////////////////////////////////////////////////////////
+            io.emit('join',msg, socket.userName);
+
+
+
+            io.emit('nafn', nick);
+///////////////////////////////////////////////////////////////////////////////////////////////
+
+
             // látum alla clienta hafa fylkið okkar í núverandi mynd
             io.emit('users_logged_in', notendur);
+            chatlog();
         });
 
-        socket.on('disconnect', function()
+        socket.on('disconnect', function(nick, msg)
         {
             console.log('A user has disconnected');
             // lækkum fjöldi_notenda úr því einn maður er farinn út
             fjoldi_notenda--;
+
+            io.emit('disconnect',msg, socket.userName);//ekki færa röð skiftir miklu máli
+            // socket.userName = nick;
+
+            //////////////////////////////////////////////////////////////////////7
             // látum clientinn vita að fjoldi_notenda hafi breyst, client þarf þá að hlusta 
             // eftir 'count' atburði og gera eitthvað við breytuna sem við sendum honum, 
             // sem sagt láta hana sjást einhversstaðar
@@ -97,14 +126,14 @@ app.get('/*', function(req, res)
         });
         socket.on('chat message', function(msg)
         {
-            // chatdb.collection("messages").insertOne({msg:socket.userName+' wrote: '+msg});
+            chatdb.collection("messages").insertOne({msg:socket.userName + ': ' + msg});
             io.emit('chat message', msg, socket.userName);
         });
     });
-// });
+});
 
 http.listen(port, function()
 {
-	console.log('Server is listening on port ' + port);
+	console.log('Server is listening on port: ' + port);
 });
 
